416d6fc3b6442f57564365a56bc4041e8413102c
Javier Guerrero <jeguerrerocordoba@gmail.com>
2025-11-22 03:55:53 -0500

    Delete redundant username in Activatedto

--- src/main/java/com/epam/gym/dto/request/ActivateDeactivateRequest.java
+++ src/main/java/com/epam/gym/dto/request/ActivateDeactivateRequest.java
@@ -1,6 +1,5 @@
 package com.epam.gym.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,9 +11,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ActivateDeactivateRequest {
-    @NotBlank(message = "Username is required")
-    private String username;
-
     @NotNull(message = "Is Active is required")
     private Boolean isActive;
 }
