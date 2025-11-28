8e9e0a039a35fc9c708895a34869ddb48105568e
Javier Guerrero <jeguerrerocordoba@gmail.com>
2025-11-28 11:30:51 -0500

    Add request validations

--- src/main/java/com/epam/gym/dto/request/TraineeRegistrationRequest.java
+++ src/main/java/com/epam/gym/dto/request/TraineeRegistrationRequest.java
@@ -1,13 +1,14 @@
 package com.epam.gym.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
 
 import java.util.Date;
 
 public record TraineeRegistrationRequest(
     @NotBlank(message = "First name is required") String firstName,
     @NotBlank(message = "Last name is required") String lastName,
-    Date dateOfBirth,
+    @PastOrPresent(message = "Date of birth cannot be in the future") Date dateOfBirth,
     String address
 ) {}
 
