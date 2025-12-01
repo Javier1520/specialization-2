fab24d500c7010c0b838b1b5bc31814766f0e90d
Javier Guerrero <jeguerrerocordoba@gmail.com>
2025-12-01 03:01:15 -0500

    Add info to swagger

--- src/main/java/com/epam/gym/config/OpenApiSecurityConfig.java
+++ src/main/java/com/epam/gym/config/OpenApiSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.epam.gym.config;
 
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,23 +17,44 @@ public class OpenApiSecurityConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-            .components(new Components()
+            .info(getInfo())
+            .components(getComponents())
+            .addSecurityItem(getSecurityItem()
+            );
+    }
+
+    private Info getInfo() {
+        return new Info()
+                .title("Gym CRM")
+                .description("REST API for managing gym trainers, trainees and training sessions")
+                .version("6.0.0")
+                .contact(new Contact()
+                        .name("Javier Guerrero")
+                        .email("javier.guerrero@email.com"))
+                .license(new License()
+                        .name("Apache 2.0")
+                        .url("https://www.apache.org/licenses/LICENSE-2.0.html"));
+    }
+
+    private Components getComponents() {
+        return new Components()
                 .addSecuritySchemes("Username",
-                    new SecurityScheme()
-                        .type(SecurityScheme.Type.APIKEY)
-                        .in(SecurityScheme.In.HEADER)
-                        .name("username")
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name("username")
                 )
                 .addSecuritySchemes("Password",
-                    new SecurityScheme()
-                        .type(SecurityScheme.Type.APIKEY)
-                        .in(SecurityScheme.In.HEADER)
-                        .name("password")
-                )
-            )
-            .addSecurityItem(new SecurityRequirement()
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name("password")
+                );
+    }
+
+    private SecurityRequirement getSecurityItem() {
+        return new SecurityRequirement()
                 .addList("Username")
-                .addList("Password")
-            );
+                .addList("Password");
     }
 }
