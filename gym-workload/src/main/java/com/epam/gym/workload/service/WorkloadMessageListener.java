aaf9bc40470ed29dad9bc7cd2340feb39b339c28
Javier Guerrero <jeguerrerocordoba@gmail.com>
2026-03-15 13:06:37 -0500

    Add Transactional annotation in WorkloadMessageListener to prevent working hours double counted

--- gym-workload/src/main/java/com/epam/gym/workload/service/WorkloadMessageListener.java
+++ gym-workload/src/main/java/com/epam/gym/workload/service/WorkloadMessageListener.java
@@ -7,10 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
+@Transactional
 public class WorkloadMessageListener {
 
     private final WorkloadService workloadService;
